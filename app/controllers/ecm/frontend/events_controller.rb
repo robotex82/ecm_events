8ef8ea7f62770de648fc78e5c33e1b1fd57409e9
Roberto Vasquez Angel <robotex@robotex.de>
2011-07-20 00:06:36 +0200

    Added calendar view

--- app/controllers/ecm/frontend/events_controller.rb
+++ app/controllers/ecm/frontend/events_controller.rb
@@ -1,6 +1,22 @@
 class Ecm::Frontend::EventsController < Ecm::FrontendController
-  def index
-    @events = ::Event.in_month(params[:year], params[:month]).all
+  def index    
+    @month = params[:month] ||= Time.now.month
+    @year  = params[:year]  ||= Time.now.year
+    
+    @time = Time.new(@year.to_i, @month.to_i)    
+    
+    @events = ::Event.in_month(@year, @month).all
+    
+    @days = {}
+    (1..Time.days_in_month(@month.to_i, @year.to_i)).each do |day| 
+      @days[day] = []
+      @events.each do |event|
+        
+        if event.begin_at > DateTime.new(@year.to_i, @month.to_i, day) and event.begin_at < DateTime.new(@year.to_i, @month.to_i, day).end_of_day
+          @days[day] << event
+        end
+      end
+    end
   end
   
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
  Rails.application.routes.draw do |map|
   resources :events, :controller => 'ecm/frontend/events', :only => [:index, :show] do
     collection do
-      get '/:year/:month', :action => :index
+      get '/:year/:month', :action => :index, :as => :calendar
     end
     # insert code for :year and :month lookup here
   end
