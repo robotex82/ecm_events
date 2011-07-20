8b79c67413c7170e918498050f1d7c7ff55cb9b0
Roberto Vasquez Angel <robotex@robotex.de>
2011-07-20 14:25:20 +0200

    Another try at timezone fixing

--- app/controllers/ecm/frontend/events_controller.rb
+++ app/controllers/ecm/frontend/events_controller.rb
@@ -11,8 +11,7 @@ class Ecm::Frontend::EventsController < Ecm::FrontendController
     (1..Time.days_in_month(@month.to_i, @year.to_i)).each do |day| 
       @days[day] = []
       @events.each do |event|
-        
-        if event.begin_at >= DateTime.new(@year.to_i, @month.to_i, day).in_time_zone.beginning_of_day.utc and event.begin_at < DateTime.new(@year.to_i, @month.to_i, day).in_time_zone.end_of_day.utc
+        if event.begin_at >= DateTime.new(@year.to_i, @month.to_i, day).in_time_zone.beginning_of_day and event.begin_at < DateTime.new(@year.to_i, @month.to_i, day).in_time_zone.end_of_day
           @days[day].push event
         end
       end
